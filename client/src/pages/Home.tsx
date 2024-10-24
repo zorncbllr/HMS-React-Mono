18abc2e44e03959b7aaecc8cb19e4ddaf59a2380
Manuel <caballeromanuel203@gmail.com>
2024-10-24 22:43:21 +0800

    testing out shadcn

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,35 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./../assets/react.svg";
-import viteLogo from "/vite.svg";
-import { useMsg } from "../services/queries";
-import { Button } from "@/components/ui/button";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 function Home() {
-  const [count, setCount] = useState(0);
-  const { data } = useMsg();
-
   return (
-    <div className="h-full flex flex-col gap-6 justify-center items-center">
-      <div className="flex gap-4">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="w-32" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img
-            src={reactLogo}
-            className="w-32 animate-pulse"
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h1 className="text-5xl font-semibold">Mono + React</h1>
-      <div className="flex gap-12 flex-col items-center">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p className="text-gray-600 text-l">{data?.msg}</p>
-      </div>
-    </div>
+    <>
+      <SidebarProvider>
+        <AppSidebar />
+        <main>
+          <SidebarTrigger />
+        </main>
+      </SidebarProvider>
+    </>
   );
 }
 
